645149166ffe128dc6c821bbc4b191a7280222c7
Nastassial <nastassialmezhen@gmail.com>
2024-01-20 16:14:53 +0300

    Добавьте файлы проекта.

---
+++ ConsoleIEnumerable/Extensions.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Numerics;
+
+namespace ConsoleIEnumerable;
+
+internal static class Extensions
+{
+    public static IEnumerable<TResult> SumWithNext<TResult>(this IEnumerable<TResult> source)
+    where TResult : INumberBase<TResult>
+    {
+        var sourceArray = source.ToArray();
+
+        for (int i = 0; i < sourceArray.Length; i++)
+        {
+            if (i == sourceArray.Length - 1)
+            {
+                yield break;
+            }
+
+            yield return sourceArray[i] + sourceArray[i + 1];
+        }
+    }
+}
---
+++ ConsoleIEnumerable/Program.cs
@@ -0,0 +1,32 @@
+﻿int[] numbersArr = [4, 2, 6, 1, 3, 2, 1];
+
+Numbers numbers = new Numbers(numbersArr);
+
+foreach (int n in numbers)
+{
+    Console.WriteLine(n);
+}
+
+
+class Numbers
+{
+    private int[] _arr;
+
+    public Numbers(int[] arr)
+    {
+        _arr = arr;
+    }
+
+    public IEnumerator<int> GetEnumerator()
+    {
+        for (int i = 0; i < _arr.Length; i++)
+        {
+            if (i == _arr.Length - 1)
+            {
+                yield break;
+            }
+
+            yield return _arr[i] + _arr[i + 1];
+        }
+    }
+}
